035b4e605a68cbbb153a65289ab2c3cf03de2bf9
Koptelove <Koptel007@yandex.ru>
2022-05-27 17:24:40 +0300

    adds a link to detailed project 💽

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Detailed project
+
+[Task-list](https://koptelove.github.io/task-list/)
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:KopteLove/task-list.git master:gh-pages
+
+cd -
